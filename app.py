252a3d0756987ce73797aa0bf4daa8ce186609e9
Hari <66300509+hareeshvp@users.noreply.github.com>
2024-01-03 11:31:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from config import vuln_app
 import os
+#import seeker
 
 '''
  Decide if you want to server a vulnerable version or not!
@@ -11,6 +12,7 @@ vuln = int(os.getenv('vulnerable', 1))
 # token alive for how many seconds?
 alive = int(os.getenv('tokentimetolive', 60))
 
+#seeker.start()
 
 # start the app with port 5000 and debug on!
 if __name__ == '__main__':
